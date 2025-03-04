99bc935b52af8ac4d910d9c616b1f292999db8e7
Harikrishna772003 <151329276+Harikrishna772003@users.noreply.github.com>
2025-03-04 12:02:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import time
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
 import warnings
